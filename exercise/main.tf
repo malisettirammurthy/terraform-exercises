09154e1579db27df4557fdd0fe1c07a5f7a9400d
Ram Murthy <malisettirammurthy@gmail.com>
2022-06-20 10:13:49 +0000

    Exercise02: Use .tfvars and variables.tf

--- exercise/main.tf
+++ exercise/main.tf
@@ -1,3 +1,4 @@
+
 resource "aws_s3_object" "student_alias" {
   bucket = "sm-di-${var.student_alias}"
   key = "student.alias"
--- exercise/variables.tf
+++ exercise/variables.tf
@@ -1,3 +1,5 @@
 variable "student_alias" {
   description = "participant-07"
+  default = "participant-07-default-value"
+  type = string
 }
