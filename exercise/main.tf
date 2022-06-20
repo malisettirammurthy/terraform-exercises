eacfdc68584d37dcc3b7c0d560a12738aa716cb9
Ram Murthy <malisettirammurthy@gmail.com>
2022-06-20 09:44:02 +0000

    Exercise02: variable stanza

--- exercise/main.tf
+++ exercise/main.tf
@@ -1,6 +1,6 @@
 resource "aws_s3_object" "student_alias" {
-  bucket = "sm-di-participant-07"
+  bucket = "sm-di-${var.student_alias}"
   key = "student.alias"
-  content = "This bucket is reserved for participant-07"
+  content = "This bucket is reserved for ${var.student_alias}"
 }
 
